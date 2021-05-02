6d84300427d491fe798febb7efd3233eef6b1596
casseDeChameau <48219695+casseDeChameau@users.noreply.github.com>
2021-05-02 09:58:54 +0200

    commit 2; single project ok, timeline ajoutée sur homepage; todo: changer fond scroll timeline, les mettre en bg de nav-timeline,, mettre timeline nav en pos-r, supp section, changer resolution images, faire gif pour ecran pc single page.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@
 let lastScrollY;
 let projectArray = Array.from($('.project'));
 let portfolio = document.querySelector('.portfolio');
+let competencesWrap = document.querySelector('.competences-wrapper');
+let competencesCont = document.querySelector('.competences-container');
+let competences = document.querySelector('.competences');
+let timeline = document.querySelector('.timeline');
+// todo: mettre adresse à jour quand sur serveur
+let homePageUrl = 'http://127.0.0.1:5501/';
+
 
 // ! -----------------------    FUNCTIONS
 function showProjectInfosOnMobile() {
@@ -9,7 +16,7 @@ function showProjectInfosOnMobile() {
         let projectTop = portfolio.offsetTop + p.offsetTop;
         let projectBot = projectTop + p.offsetHeight;
         let currentMiddle = scrollY + (window.innerHeight / 2);
-        if (projectTop < currentMiddle && projectBot > currentMiddle) {
+        if ((projectTop - (window.innerHeight / 4)) < currentMiddle && (projectBot + (window.innerHeight / 6)) > currentMiddle) {
             p.classList.add('show-info');
         } else {
             p.classList.remove('show-info');
@@ -21,13 +28,34 @@ function showProjectInfosOnMobile() {
 //? effet liés au scroll
 window.addEventListener('scroll', () => {
     let currentScroll = scrollY;
-    //? réduction taille navbar si sur home page
-    // todo: mettre adresse à jour quand sur serveur
-    if (window.location.href == 'http://127.0.0.1:5501/index.html') {
+    //! ---------------------------    sur home page
+    if (window.location.href.includes(homePageUrl)) {
+        //? comportement de la navbar au scroll up/down
         currentScroll > lastScrollY ? $('.main-nav').addClass('on-scroll') : $('.main-nav').removeClass('on-scroll');
 
+        // console.log(scrollY, $('.competences-wrapper').offset().top);
+
+        //? comportement .competences quand arrive à timeline
+        //* 1. reduction div compétences 
+        if (currentScroll > competencesWrap.getBoundingClientRect().top + 1000) {
+            competencesWrap.classList.add('next-timeline');
+        } else {
+            competencesWrap.classList.remove('next-timeline');
+        }
+        //* 2. affichage nav timeline
+        if (currentScroll > competencesWrap.getBoundingClientRect().top + 1500) {
+            setTimeout(() => {
+                timeline.classList.add('show-timeline-nav');
+            }, 1000);
+        } else if (currentScroll > competencesWrap.getBoundingClientRect().bottom - window.innerHeight) {
+            timeline.classList.remove('show-timeline-nav');
+        }
+
+
+
+
+        //? masque la navbar lorsqu'on est sur la partie gallery
         if (currentScroll > ($('.skills-logos').offset().top - 50)) {
-            //? masque la navbar lorsqu'on est sur la partie gallery
             $('.main-nav').addClass('on-gallery');
             $('.skills-logos').addClass('on-top');
         } else {
@@ -37,11 +65,12 @@ window.addEventListener('scroll', () => {
         //? affiche les infos des projets au scroll à défaut du hover en mobile
         showProjectInfosOnMobile();
 
-    } else {
-        //! effet pour toute autre page que index
+    }
+    //! ------------------------------- PAS sur homepage
+    else {
         currentScroll > lastScrollY ? $('.main-nav').addClass('on-project-scroll') : $('.main-nav').removeClass('on-project-scroll');
     }
-
+    //? enregistre la dernière position pour savoir direction du scroll 
     lastScrollY = currentScroll;
 });
 
@@ -54,4 +83,28 @@ $('.burger-ico').click(() => {
 $('.all-proj').click(() => {
     $('.proj-thumbnails').toggleClass('on-click')
     $('.all-proj').toggleClass('on-click')
-})
+});
+
+//? timeline plugin -  SMOOTH SCROLLING SECTIONS
+
+$('a[href*=#]:not([href=#])').click(function() {
+    // $('.timeline-plug-in a href').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+        location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                // $('.timeline-plug-in').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+// let timelineNavLink = Array.from(document.querySelector('.timeline-plug-in nav a'));
+// console.log(timelineNavLink);
+// $('.timeline-plug-in nav a').click(() => {
+
+// })
